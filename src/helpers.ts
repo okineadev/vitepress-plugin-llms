8ab97c6d4b3a16523479c566b5e0206954e09086
kravetsone <57632712+kravetsone@users.noreply.github.com>
2025-03-19 14:07:33 +0300

    fix: 🚑 fix TOC link generation to ensure consistent path formatting on windows and linux

--- src/helpers.ts
+++ src/helpers.ts
@@ -28,7 +28,7 @@ export function generateTOC(preparedFiles: PreparedFile[]) {
 
 	for (const file of preparedFiles) {
 		const relativePath = path.relative(process.cwd(), file.path)
-		tableOfContent += `- [${file.title}](/${stripExt(relativePath)}.md)\n`
+		tableOfContent += `- [${file.title}](/${stripExt(relativePath).replace(/\\/g, '/')}.md)\n`
 	}
 
 	return tableOfContent

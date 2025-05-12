6c6804a0a89dc33d6dee33be21b2f7cbf10197d5
arshad <arshadpyaseen@gmail.com>
2025-05-13 05:10:36 +0530

    chore: upgrade bunup and add report plugin

--- bunup.config.ts
+++ bunup.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'bunup'
+// @ts-ignore
+import { report } from 'bunup/plugins'
 
 export default defineConfig({
 	entry: ['src/index.ts'],
 	format: ['esm'],
-	// Generate declaration file (.d.ts)
 	dts: true,
 	minify: true,
 	banner: '// Built with bunup',
 	clean: true,
+	plugins: [report()],
 })

fe1d5f5aff5b2f9ade73050ecf9118e05ab3dfc6
Okinea Dev <hi@okinea.dev>
2025-05-13 08:37:04 +0200

    return `dts` comment

--- bunup.config.ts
+++ bunup.config.ts
@@ -5,6 +5,7 @@ import { report } from 'bunup/plugins'
 export default defineConfig({
 	entry: ['src/index.ts'],
 	format: ['esm'],
+	// Generate declaration file (.d.ts)
 	dts: true,
 	minify: true,
 	banner: '// Built with bunup',

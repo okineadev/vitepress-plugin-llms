66d43382f8e96befb0550cd85616f7599287b706
Okinea Dev <hi@okinea.dev>
2025-12-26 18:19:06 +0100

    chore: fix Renovate commit message types

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,6 +1,8 @@
 import type { Configuration } from 'lint-staged'
 
-export default {
+const config: Configuration = {
 	'*.{ts,vue}': () => ['bun run lint', 'bun run lint:tsc'],
 	'*': ['bun run format --no-errors-on-unmatched', 'cspell --no-error-on-empty'],
-} as Configuration
+}
+
+export default config

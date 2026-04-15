e043c644bb2e0dafe869d34098360938a92cb013
Isa <hi@okinea.dev>
2026-04-15 18:50:40 +0000

    🗺️ ci(pkg-pr-new): ship sourcemaps for `pkg-pr-new` builds
    Maybe this will somehow help with debugging, especially since these are preview builds.

--- bunup.config.ts
+++ bunup.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
 		splitting: true,
 		resolve: false,
 	},
+	// @ts-expect-error
+	// oxlint-disable-next-line typescript/strict-boolean-expressions
+	sourcemap: process.env.PKG_PR_NEW ? 'external' : false,
 	banner: '// Built with bunup (https://bunup.dev)',
 	plugins: [
 		copy('src/vitepress-components/*.vue').to('vitepress-components'),
